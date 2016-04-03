5050ab86dc12a94c47164972054c4383afb5442f
Dimitar Angelov <dimitar.angelov@menasoftware.com>
2016-04-03 17:24:17 +0300

    Create laser bolt.

---
+++ Assets/Scripts/BoltMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BoltMove : MonoBehaviour
+{
+
+    #region Properties
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    #endregion
+
+    #region Events
+
+    // Use this event for initialization.
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.velocity = transform.forward * speed;
+    }
+
+    #endregion
+
+}
