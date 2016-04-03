68df96b2cd236bd58f94f850788ce94fa3b0ad70
Dimitar Angelov <dimitar.angelov@menasoftware.com>
2016-04-03 16:19:23 +0300

    Move the player (and constraint it to the game view).

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,5 @@
+﻿[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    #region Properties
+
+    public float speed, tilt;
+    public Boundary boundary;
+
+    private Rigidbody rb;
+
+    #endregion
+
+    #region Events
+
+    // Use this event for initialization.
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // FixedUpdate is called just before performing any physics calculations.
+    void FixedUpdate()
+    {
+        // Move the ship...
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        rb.velocity = movement * speed;
+
+        // ... and constrain it to the game view
+        rb.position = new Vector3(
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        // Then, tilt the ship while moving left and right
+        rb.rotation = Quaternion.Euler(0f, 0f, rb.velocity.x * -tilt);
+    }
+
+    #endregion
+
+}
