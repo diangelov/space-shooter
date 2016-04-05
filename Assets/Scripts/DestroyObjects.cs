141b7c1dc2c6c434b057a8c83b9f44d1ea2f9aac
Dimitar Angelov <dimitar.angelov@menasoftware.com>
2016-04-05 13:05:26 +0300

    Destroy the objects when they leave the game area.

---
+++ Assets/Scripts/DestroyObjects.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DestroyObjects : MonoBehaviour
+{
+
+    #region Events
+
+    // OnTriggerExit is called when the Collider has stopped touching the trigger.
+    public void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+    #endregion
+
+}
