24222028f55dd9afbe1029a810358e1d109668e0
Dimitar Angelov <dimitar.angelov@menasoftware.com>
2016-04-05 14:39:57 +0300

    Create asteroid, then tumble and destroy it.

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+
+    #region Events
+
+    // OnTriggerEnter is called when the Collider enters the trigger.
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!other.tag.Equals("Boundary"))
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+
+    #region Properties
+
+    public float maxTumble = 0;
+
+    private Rigidbody rb;
+
+    #endregion
+
+    #region Events
+
+    // Use this event for initialization.
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.angularVelocity = Random.insideUnitSphere * maxTumble;
+    }
+
+    #endregion
+
+}
