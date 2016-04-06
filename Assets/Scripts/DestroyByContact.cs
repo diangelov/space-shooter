64cd3d882430ef6f0390aab43b819982d1526836
Dimitar Angelov <dimitar.angelov@menasoftware.com>
2016-04-06 14:04:48 +0300

    Add explosion VFXs to the asteroid and player.

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,15 +3,33 @@
 public class DestroyByContact : MonoBehaviour
 {
 
+    #region Properties
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    #endregion
+
     #region Events
 
     // OnTriggerEnter is called when the Collider enters the trigger.
     public void OnTriggerEnter(Collider other)
     {
-        if (!other.tag.Equals("Boundary"))
+        // Ignore the Boundary events
+        if (other.tag.Equals("Boundary"))
         {
-            Destroy(gameObject);
-            Destroy(other.gameObject);
+            return;
+        }
+
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+
+        // Run explosion VFXs
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        if (other.tag.Equals("Player"))
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
         }
     }
 
