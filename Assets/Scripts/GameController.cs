f7c9a17da15f1e1bc4f8655fde46acfd0243c894
Dimitar Angelov <dimoburg@abv.bg>
2016-04-15 01:58:57 +0300

    Spawn asteroid from random location

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    #region Properties
+
+    public GameObject hazard;
+    public Vector3 spawnInitLocation;
+
+    #endregion
+
+    #region Events
+
+    // Use this event for initialization.
+    void Start()
+    {
+        SpawnWaves();
+    }
+
+    void SpawnWaves()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnInitLocation.x, spawnInitLocation.x),
+            spawnInitLocation.y, spawnInitLocation.z);
+
+        // No rotation
+        Quaternion spawnRotation = Quaternion.identity;
+
+        Instantiate(hazard, spawnPosition, spawnRotation);
+    }
+
+    #endregion
+
+}
